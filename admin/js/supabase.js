166aee4fcb67f9837ad135a21eed44fa08c6c0b1
MASK INTELLIGENCE <maskintelligence@gmail.com>
2026-01-12 15:01:27 +0300

    Create supabase.js

---
+++ admin/js/supabase.js
@@ -0,0 +1,6 @@
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+
+export const supabase = createClient(
+  "https://cvziqzkbpcwjzvhbwtpk.supabase.co",
+  "sb_publishable_PdCTyA2Eqcm4t_tFJv9cqw_QSq8dxR0"
+);
