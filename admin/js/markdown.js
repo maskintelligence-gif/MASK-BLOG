99c284e69d7d6c83eb3926fe084f4b4b6d903ba4
MASK INTELLIGENCE <maskintelligence@gmail.com>
2026-01-12 16:03:21 +0300

    Create markdown.js

---
+++ admin/js/markdown.js
@@ -0,0 +1,10 @@
+export function renderMarkdown(md) {
+  return md
+    .replace(/^### (.*$)/gim, "<h3>$1</h3>")
+    .replace(/^## (.*$)/gim, "<h2>$1</h2>")
+    .replace(/^# (.*$)/gim, "<h1>$1</h1>")
+    .replace(/\*\*(.*)\*\*/gim, "<strong>$1</strong>")
+    .replace(/\*(.*)\*/gim, "<em>$1</em>")
+    .replace(/`([^`]+)`/gim, "<code>$1</code>")
+    .replace(/\n$/gim, "<br />");
+}
